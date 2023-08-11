cb0d96cbf5d76cb5abbc7c1b539640fffabdc936
TarunDeveloper01 <tarundjrock44@gmail.com>
2023-08-11 18:47:04 +0530

    second

--- src/features/Product-list/ProductList.js
+++ src/features/Product-list/ProductList.js
@@ -13,6 +13,26 @@ const products = [
     price: "$35",
     color: "Black",
   },
+  {
+    id: 2,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-04.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$25",
+    color: "Blue",
+  },
+  {
+    id: 3,
+    name: "Basic Tee",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg",
+    imageAlt: "Front of men's Basic Tee in black.",
+    price: "$30",
+    color: "Red",
+  },
 ];
 
 export default function ProductList() {
@@ -24,7 +44,7 @@ export default function ProductList() {
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-            Customers also purchased
+            Products
           </h2>
 
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
